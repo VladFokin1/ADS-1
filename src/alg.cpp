84225de06af8547ac34043e4383c57ab76c3a3c6
VladFokin1 <94217312+VladFokin1@users.noreply.github.com>
2022-02-23 13:11:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,7 +6,7 @@
 
 
 bool checkPrime(uint64_t value) {
-  for (int i = 2; i < sqrt(value); i++) {
+  for (int i = 2; i <= sqrt(value); i++) {
     if (value%i == 0) {
       return false;
             }
